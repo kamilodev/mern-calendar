e4b503c5fa1125436ba8447ca89c4ed398c76765
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-21 00:20:48 +0200

    Configuring styles for BigCalendar

--- src/calendar/CalendarScreen.js
+++ src/calendar/CalendarScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { Navbar } from '../ui/Navbar'
+import { messages } from '../helpers/calendar-messages'
 import moment from 'moment'
+import 'moment/locale/es'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-import { Navbar } from '../ui/Navbar'
-
+moment.locale('es')
 const localizer = momentLocalizer(moment)
 
 const events = [
@@ -13,10 +15,22 @@ const events = [
 		start: moment().toDate(),
 		end: moment().add(2, 'hours').toDate(),
 		bgcolor: '#fafafa',
+		notes: 'Comprar la tarta',
 	},
 ]
 
 export const CalendarScreen = () => {
+	const eventGetter = (event, start, end, isSelected) => {
+		const style = {
+			backgroundColor: '#C6D13B',
+			borderRadius: '0px',
+			opacity: 0.8,
+			display: 'block',
+			color: '#241B45',
+		}
+		return { style }
+	}
+
 	return (
 		<div className='calendar__screen'>
 			<Navbar />
@@ -26,6 +40,8 @@ export const CalendarScreen = () => {
 				events={events}
 				startAccesor='start'
 				endAccesor='end'
+				messages={messages}
+				eventPropGetter={eventGetter}
 			/>
 		</div>
 	)
