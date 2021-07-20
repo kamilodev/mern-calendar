7014717bea13f45dc51576e135a17245a2e1311e
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-20 23:56:18 +0200

    Installing and configuring BigCalendar

--- src/calendar/CalendarScreen.js
+++ src/calendar/CalendarScreen.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
 import { Navbar } from '../ui/Navbar'
+
+const localizer = momentLocalizer(moment)
+
+const events = [
+	{
+		title: 'Cumpleaños de jefe',
+		start: moment().toDate(),
+		end: moment().add(2, 'hours').toDate(),
+		bgcolor: '#fafafa',
+	},
+]
 
 export const CalendarScreen = () => {
 	return (
-		<div>
+		<div className='calendar__screen'>
 			<Navbar />
+
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccesor='start'
+				endAccesor='end'
+			/>
 		</div>
 	)
 }
