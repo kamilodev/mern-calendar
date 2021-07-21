7be0c5aeda9b598916c6ac94ad3caa82bdeaa42f
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-21 21:38:18 +0200

    Validating form and changing structure project

--- src/calendar/CalendarModal.js
+++ src/calendar/CalendarModal.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
-import { EventModal } from '../ui/EventModal'
+import DateTimePicker from 'react-datetime-picker'
+import moment from 'moment'
+import Swal from 'sweetalert2'
 
 const customStyles = {
 	content: {
@@ -16,7 +18,71 @@ const customStyles = {
 Modal.setAppElement('#root')
 
 export const CalendarModal = () => {
-	const closeModal = () => {}
+	const closeModal = () => {
+		console.log('Me han llamado tio')
+	}
+
+	const now = moment().minutes(0).seconds(0).add(1, 'hours') // 3:00:00
+	const nowPlus1 = now.clone().add(1, 'hours')
+
+	const [dateStart, setDateStart] = useState(now.toDate())
+	const [dateEnd, setDateEnd] = useState(nowPlus1.toDate())
+	const [titleValid, setTitleValid] = useState(true)
+
+	const initEvent = {
+		title: '',
+		notes: '',
+		start: now.toDate(),
+		end: nowPlus1.toDate(),
+	}
+
+	const [formValues, setFormValues] = useState(initEvent)
+	const { notes, title, start, end } = formValues
+
+	const handleInputChange = ({ target }) => {
+		setFormValues({
+			...formValues,
+			[target.name]: target.value,
+		})
+	}
+
+	const handleStartDateChange = e => {
+		setDateStart(e)
+		setFormValues({
+			...formValues,
+			start: e,
+		})
+	}
+
+	const handleEndDateChange = e => {
+		setDateEnd(e)
+		setFormValues({
+			...formValues,
+			end: e,
+		})
+	}
+
+	const handleSubmitForm = e => {
+		e.preventDefault()
+
+		const momentStart = moment(start)
+		const momentEnd = moment(end)
+
+		if (momentStart.isSameOrAfter(momentEnd)) {
+			return Swal.fire(
+				'Error',
+				'La fecha fin debe de ser mayor a la fecha de inicio',
+				'error',
+			)
+		}
+
+		if (title.trim().length < 2) {
+			return setTitleValid(false)
+		}
+
+		setTitleValid(true)
+		closeModal()
+	}
 
 	return (
 		<Modal // Working with Redux, not useState
@@ -27,7 +93,68 @@ export const CalendarModal = () => {
 			closeTimeoutMS={400}
 			className='modal __modal-main mt-2'
 			overlayClassName='modal-fondo'>
-			<EventModal />
+			<h1 className='__modal-h1 mt-1'> Nuevo evento </h1>
+			<hr />
+			<form className='container' onSubmit={handleSubmitForm}>
+				<div className='form-group'>
+					<label className='mt-2'>Fecha y hora inicio</label>
+					<DateTimePicker
+						onChange={handleStartDateChange}
+						value={dateStart}
+						className='form-control'
+					/>
+				</div>
+
+				<div className='form-group'>
+					<label className='mt-2'>Fecha y hora fin</label>
+					<DateTimePicker
+						onChange={handleEndDateChange}
+						value={dateEnd}
+						minDate={dateStart}
+						className='form-control'
+					/>
+				</div>
+
+				<hr />
+				<div className='form-group'>
+					<label className='mt-2'>Titulo y notas</label>
+					<input
+						type='text'
+						className={`form-control ${
+							!titleValid && 'is-invalid'
+						}`}
+						placeholder='Título del evento'
+						name='title'
+						autoComplete='off'
+						value={title}
+						onChange={handleInputChange}
+					/>
+					<small id='emailHelp' className='form-text text-muted'>
+						Una descripción corta
+					</small>
+				</div>
+
+				<div className='form-group'>
+					<textarea
+						type='text'
+						className='form-control'
+						placeholder='Notas'
+						rows='5'
+						name='notes'
+						value={notes}
+						onChange={handleInputChange}></textarea>
+					<small id='emailHelp' className='form-text text-muted'>
+						Información adicional
+					</small>
+				</div>
+
+				<button
+					type='submit'
+					className='btn btn-outline-primary btn-block'>
+					<i className='far fa-save'></i>
+					<span> Guardar</span>
+				</button>
+			</form>
 		</Modal>
 	)
 }
