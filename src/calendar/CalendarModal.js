10e1bb79e8856885ecfb1c63a97d5c14f517a0c1
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-21 15:36:43 +0200

    Extract the information entered by the user in the EventModal form

--- src/calendar/CalendarModal.js
+++ src/calendar/CalendarModal.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import Modal from 'react-modal';
+import React from 'react'
+import Modal from 'react-modal'
 import { EventModal } from '../ui/EventModal'
 
 const customStyles = {
 	content: {
-		top			:'50%',
-		left		:'50%',
-		right		:'auto',
-		bottom		:'auto',
-		marginRight	:'-50%',
-		transform	:'translate(-50%, -50%)'
-	}
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
 }
 
 Modal.setAppElement('#root')
 
 export const CalendarModal = () => {
-
-	const closeModal = () => {
-
-	}
+	const closeModal = () => {}
 
 	return (
 		<Modal // Working with Redux, not useState
@@ -29,9 +26,8 @@ export const CalendarModal = () => {
 			style={customStyles}
 			closeTimeoutMS={400}
 			className='modal __modal-main mt-2'
-			overlayClassName='modal-fondo'
-		>
+			overlayClassName='modal-fondo'>
 			<EventModal />
 		</Modal>
-	);
-};
+	)
+}
--- src/calendar/CalendarScreen.js
+++ src/calendar/CalendarScreen.js
@@ -1,44 +1,29 @@
-import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import { Navbar } from '../ui/Navbar';
-import { messages } from '../helpers/calendar-messages';
-import { CalendarEvent } from './CalendarEvent';
-import { CalendarModal } from './CalendarModal';
-import moment from 'moment';
-import 'moment/locale/es';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-
-moment.locale('es');
-const localizer = momentLocalizer(moment);
-
-const events = [
-	{
-		title: 'Cumpleaños del jefe',
-		start: moment().toDate(),
-		end: moment().add(2, 'hours').toDate(),
-		bgcolor: '#fafafa',
-		notes: 'Comprar la tarta',
-		user: {
-			_id: '123',
-			name: 'Kamilo',
-		},
-	},
-];
+import React, { useState } from 'react'
+import { Calendar } from 'react-big-calendar'
+import { Navbar } from '../ui/Navbar'
+import { messages } from '../helpers/calendar-messages'
+import { CalendarEvent } from './CalendarEvent'
+import { CalendarModal } from './CalendarModal'
+import { events, localizer } from '../helpers/calendar-events-config'
+import 'moment/locale/es'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 export const CalendarScreen = () => {
-	const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
-	const onDoubleClick = (e) => {
-		console.log(e);
-	};
+	const [lastView, setLastView] = useState(
+		localStorage.getItem('lastView') || 'month',
+	)
+	const onDoubleClick = e => {
+		console.log(e)
+	}
 
-	const onSelectEvent = (e) => {
-		console.log(e);
-	};
+	const onSelectEvent = e => {
+		console.log(e)
+	}
 
-	const onViewChange = (e) => {
-		setLastView(e);
-		localStorage.setItem('lastView', e);
-	};
+	const onViewChange = e => {
+		setLastView(e)
+		localStorage.setItem('lastView', e)
+	}
 
 	const eventGetter = (event, start, end, isSelected) => {
 		const style = {
@@ -47,19 +32,19 @@ export const CalendarScreen = () => {
 			opacity: 0.8,
 			display: 'block',
 			color: '#241B45',
-		};
-		return { style };
-	};
+		}
+		return { style }
+	}
 
 	return (
-		<div className="calendar__screen">
+		<div className='calendar__screen'>
 			<Navbar />
 
 			<Calendar
 				localizer={localizer}
 				events={events}
-				startAccesor="start"
-				endAccesor="end"
+				startAccesor='start'
+				endAccesor='end'
 				messages={messages}
 				eventPropGetter={eventGetter}
 				components={{
@@ -73,5 +58,5 @@ export const CalendarScreen = () => {
 
 			<CalendarModal />
 		</div>
-	);
-};
+	)
+}
