8de93ebf759747d0dc887b73dfbfdfda4473cb12
kamilo923 <kamilo_vanegas@hotmail.com>
2021-07-25 22:49:11 +0200

    Make the HTTP request to authenticate user. Save token in localStorage

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,26 @@
+import { fetchWithoutToken } from '../helpers/fetchWithoutToken'
+import { types } from '../types/types'
+
 export const startLogin = (email, password) => {
-	return async () => {
-		console.log(email, password)
+	return async dispatch => {
+		const resp = await fetchWithoutToken('auth', { email, password }, 'POST')
+		const body = await resp.json()
+
+		if (body.ok) {
+			localStorage.setItem('token', body.token)
+			localStorage.setItem('token-init-date', new Date().getTime())
+
+			dispatch(
+				login({
+					uid: body.uid,
+					name: body.name,
+				}),
+			)
+		}
 	}
 }
+
+const login = user => ({
+	type: types.authLogin,
+	payload: user,
+})
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { types } from '../types/types'
+
 const initialState = {
 	checking: true,
 	// uid: null,
@@ -6,6 +8,13 @@ const initialState = {
 
 export const authReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case types.authLogin:
+			return {
+				...state,
+				checking: false,
+				...action.payload,
+			}
+
 		default:
 			return state
 	}
